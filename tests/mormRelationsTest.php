eb45b104d1ea80ca625304f725f99c55ddc500a3
Tony Blundell <tony.blundell@kineo.com>
2013-09-09 16:05:56 +0100

    relations_single test

--- tests/mormRelationsTest.php
+++ tests/mormRelationsTest.php
@@ -43,27 +43,32 @@ class morm_relations_single_test extends advanced_testcase {
     }
 
     public function test_one_to_many_rels() {
+        // Create a user, course and course completion
         $john = $this->gen->create_user(array('username'=>'john'));
         $paul = $this->gen->create_user(array('username'=>'paul'));
         $help = $this->gen->create_course(array('shortname'=>'Help!'));
-        $john_help = new CourseCompletion_R(
+        $comp = new CourseCompletion_R(
             array('user'=>$john, 'course'=>$help)
         );
-        $john_help->save();
-        $paul_help = new CourseCompletion_R(
-            array('user'=>$paul, 'course'=>$help)
-        );
-        $paul_help->save();
+        $comp->save();
+        // Pull the objects back via morm
         $john_from_db = User_R::get_one(array('id__eq'=>$john->id));
+        $paul_from_db = User_R::get_one(array('id__eq'=>$paul->id));
         $help_from_db = Course_R::get_one(array('id__eq'=>$help->id));
-        $john_help_from_db = CourseCompletion_R::get_one(array(
+        $comp_from_db = CourseCompletion_R::get_one(array(
             'userid__eq'=>$john->id, 'course__eq'=>$help->id
         ));
-        $all_from_db = CourseCompletion_R::get();
-        $this->assertEquals(2, count($all_from_db));
-        $this->assertInstanceOf('CourseCompletion_R', $john_help_from_db);
-        $this->assertEquals($john_from_db, $john_help_from_db->user);
-        $this->assertEquals($help_from_db, $john_help_from_db->course);
+        // Assert that the relation attributes are set to the correct objects
+        $this->assertInstanceOf('CourseCompletion_R', $comp_from_db);
+        $this->assertEquals($john_from_db, $comp_from_db->user);
+        $this->assertEquals($help_from_db, $comp_from_db->course);
+        // Change the relations, re-pull, assert they are set to the new objects
+        $comp->user = $paul;
+        $comp->save();
+        $comp_from_db = CourseCompletion_R::get_One(array(
+            'id__eq'=>$comp->id
+        ));
+        $this->assertEquals($paul_from_db, $comp_from_db->user);
     } 
     
 }
