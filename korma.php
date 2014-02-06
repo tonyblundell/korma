ed6ad49e23c8ae85efb21980eff62bedd4942efb
Dominic Orme <dominic.orme@googlemail.com>
2014-02-06 16:19:07 +0000

    Comments, Whitespace and Type hinting
    Commented each function, it's parameters and return type.
    Additional whitespace where appropriate
    Removed checks for a function being an array as the type hinting will solve that issue.

--- korma.php
+++ korma.php
@@ -14,11 +14,22 @@ class Model {
     protected static $one_to_many_relations = array();
     protected static $many_to_one_relations = array();
 
-    public static function get($clauses=array(), $order='id', $limit=0, $offset=0) {
-        // Gets all matching rows from the database then returns as an array
-        // of instances of this class.
-        // See the comments within get_records_sql for details on how the
-        // parameters are interpreted.
+    /**
+     * Gets all matching rows from the database then returns as an array
+     * of instances of this class.
+     * See the comments within get_records_sql for details on how the
+     * parameters are interpreted.
+     * 
+     * @param array $clauses A key/value pair array of where clauses
+     * @param string $order  The ORDER BY column of the query
+     * @param int $limit     The number of records to return
+     * @param int $offset    The offset from the start of the table to start the query.
+     * 
+     * @return array[Model] An array of instances of the underlying model.
+     */
+    public static function get(
+        array $clauses = array(), $order = 'id', $limit = 0, $offset = 0
+    ) {
         global $DB;
         $sql = static::generate_sql($clauses, $order, $limit, $offset);
         $records = $DB->get_records_sql($sql);
@@ -26,7 +37,14 @@ class Model {
         return $instances;
     }
 
-    public static function get_one($clauses=array()) {
+    /**
+     * Returns the first found instance based on the clauses.
+     * 
+     * @param array $clauses A key/value pair array of where clauses
+     * 
+     * @return stdClass An object representing a row of the table.
+     */
+    public static function get_one(array $clauses = array()) {
         // Makes a call to get, then returns the first found instance.
         // If no matching instances are found, this would return boolean false
         // which is the result of calling reset on an empty array in PHP.
@@ -36,7 +54,15 @@ class Model {
         return reset($instances);
     }
 
-    public static function count($clauses=array()) {
+    /**
+     * Returns the number of entries that would be returned if the query
+     * with the specified clauses is run
+     * 
+     * @param array $clauses A key/value pair array of where clauses
+     * 
+     * @return int The number of records that would be returned.
+     */
+    public static function count(array $clauses = array()) {
         // See the comments within get_records_sql for details on how the
         // parameters are interpreted.
         global $DB;
@@ -48,7 +74,14 @@ class Model {
         return $DB->count_records_sql($sql);
     }
 
-    public static function delete($clauses=array()) {
+    /**
+     * Deletes a record specified by the constraints in the clauses passed in.
+     * 
+     * @param array $clauses A key/value pair array of where clauses
+     * 
+     * @return boolean TRUE always.
+     */
+    public static function delete(array $clauses = array()) {
         // See the comments within get_records_sql for details on how the
         // parameters are interpreted.
         global $DB;
@@ -58,18 +91,26 @@ class Model {
         return $DB->delete_records_select(static::$table, $clauses_sql);
     }
 
-    public function __construct($attrs=array()) {
-        // Class constructor, accepts an optional $attrs argument which should
-        // be an associative array if passed.
-        // For every item in the array, we set an attribute on the instance
-        // being constructed.
+    /**
+     * Class constructor, accepts an optional $attrs argument which should
+     * be an associative array if passed.
+     * For every item in the array, an attribute is set on the instance
+     * being constructed.
+     * 
+     * @param array $attrs
+     */
+    public function __construct(array $attrs = array()) {
         foreach($attrs as $attr => $value) {
             $this->$attr = $value;
         }
     }
  
+    /**
+     * Updates the record if it already exists, creates if not.
+     * 
+     * @return stdClass The model with its amended data.
+     */ 
     public function save() {
-        // Updates the record if it already exists, creates if not.
         // Creates a stdClass instance to give to the $DB functions, then 
         // populates it with each field value based on $this's attrs.
         // Also loops through any many-to-one relations and sets the 
@@ -98,26 +139,48 @@ class Model {
         return $this->refresh();
     }
 
+    /**
+     * Refreshes the model with the data currently in the database
+     * 
+     * @return stdClass The updated instance.
+     */ 
     public function refresh() {
         // Pulls the record from the database, updates $this's attributes
         // accordingly.
-        $got = static::get_one(array('id__eq'=>$this->id));
+        $got = static::get_one(array('id__eq' => $this->id));
         foreach (get_object_vars($got) as $key => $value) {
             $this->$key = $value;
         }
         return $this;
     }
 
+    /**
+     * Get related objects based on foreign keys on this model.
+     * 
+     * @param string $relation The relation to follow
+     * 
+     * @return array An array of related objects, each being an instance of
+     *               the specified class.
+     */
     public function get_related($relation) {
         // Returns a list of related objects, each item would be an instance
         // of the specified class.
         $rel = static::$one_to_many_relations[$relation];
         return $rel['model']::get(
-            array($rel['field'].'__eq'=>$this->id)
+            array($rel['field'].'__eq' => $this->id)
         );
     }
 
-    public function set_related($relation, $items) {
+    /**
+     * Resets all relationships so only the items specified will be
+     * related to this model.
+     * 
+     * @param string $relation The relation to set
+     * @param array  $items    The items to be newly related to the model.
+     * 
+     * @return bool TRUE always.
+     */ 
+    public function set_related($relation, array $items) {
         // First removes existing related objects (by NULLing the 'FK' field),
         // then adds the new related objects.
         global $DB;
@@ -128,7 +191,7 @@ class Model {
             WHERE {$rel['field']} = $this->id
         ");
         $ids = array();
-        foreach(is_array($items) ? $items : array($items) as $item) {
+        foreach($items as $item) {
             $ids[] = is_object($item) ? $item->id : $item; 
         }
         $DB->execute("
@@ -138,13 +201,22 @@ class Model {
         ");
     }
 
-    public function add_related($relation, $items) {
+    /**
+     * Adds the specified items as additional relations to the model.
+     * 
+     * @param string $relation The relation to change
+     * @param array  $items    The additional items to be related to
+     *                         the model
+     * 
+     * @return bool TRUE always.
+     */ 
+    public function add_related($relation, array $items) {
         // Adds a related item by setting it's 'FK' field to $this's ID.
         // Accepts an array of instances or IDs.
         global $DB;
         $rel = static::$one_to_many_relations[$relation]; 
         $ids = array();
-        foreach(is_array($items) ? $items : array($items) as $item) {
+        foreach($items as $item) {
             $ids[] = is_object($item) ? $item->id : $item; 
         }
         $DB->execute("
@@ -154,6 +226,14 @@ class Model {
         ");
     }
 
+    /**
+     * Removes the specified items from those with a relation to the model.
+     * 
+     * @param string $relation The relation to change
+     * @param array  $items    The items to remove from the relation.
+     * 
+     * @return bool TRUE always.
+     */
     public function remove_related($relation, $items) {
         // Removes the specified related objects by NULLing the 'FK' field.
         // If the field is set to NOT NULL in the database, this will error.
@@ -161,7 +241,7 @@ class Model {
         global $DB;
         $rel = static::$one_to_many_relations[$relation]; 
         $ids = array();
-        foreach(is_array($items) ? $items : array($items) as $item) {
+        foreach($items as $item) {
             $ids[] = is_object($item) ? $item->id : $item; 
         }
         $DB->execute("
@@ -171,7 +251,18 @@ class Model {
         ");
     }
 
-    private static function generate_sql($clauses, $order, $limit, $offset) {
+    /**
+     * Generates the SQL required based on passed in clauses, order, limit and
+     * offset.
+     * 
+     * @param array  $clauses The where clauses to use
+     * @param string $order   The column to order by
+     * @param int    $limit   The LIMIT statement within SQL
+     * @param int    $offset  The OFFSET statement within SQl
+     * 
+     * @return string A valid SQL statement
+     */ 
+    private static function generate_sql(array $clauses, $order, $limit, $offset) {
         // Private function for generating the SQL select clause.
         // Clauses must be an array. Items in the array are AND-ed together,
         // if an array of arrays is specified, the arrays are OR-ed.
@@ -218,7 +309,14 @@ class Model {
         return $sql;
     }
 
-    private static function generate_sql_where_clause($clauses) {
+    /**
+     * Generates a WHERE clause based on the conditions passed in.
+     * 
+     * @param array $clauses The clauses for the WHERE statement
+     * 
+     * @return string A valid WHERE statement
+     */
+    private static function generate_sql_where_clause(array $clauses) {
         // Private function for generating the 'WHERE' part of an SQL statement.
         // See the comments within generate_sql for details of how clauses
         // are interpreted.
@@ -243,6 +341,15 @@ class Model {
         return $sql;
     }
 
+    /**
+     * Parses a clause for the type of condition.
+     * Expects field to be of the form fieldname__clausetype
+     * 
+     * @param string $field The field and clause type.
+     * @param mixed  $value The value to compare to.
+     * 
+     * @return string A part of a where clause.
+     */
     private static function parse_clause($field, $value) {
         // Private function for parsing a field clause to SQL.
         // Expects the field to be given in the format fieldname__clausetype.
@@ -271,6 +378,14 @@ class Model {
         }
     }
 
+    /**
+     * Takes a record as returned by moodle and converts it into an
+     * Object related to the table it came from.
+     * 
+     * @param stdClass $record The record as returned from a moodle SQL query
+     * 
+     * @return Model An instance of the model relating to the table.
+     */
     private static function record_to_instance($record) {
         // Private function for casting a record object returned by $DB
         // to an instance of this class.
@@ -293,5 +408,4 @@ class Model {
         }
         return $instance;
     }
-
 }
